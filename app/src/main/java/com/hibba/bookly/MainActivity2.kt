e2b8e2c6612c04747e89049de2da45f38cb66a58
h-hibaaah <hibba5825@gmail.com>
2026-08-15 20:18:07 +0500

    feat: Implement Splash Screen

--- app/src/main/java/com/hibba/bookly/MainActivity2.kt
+++ app/src/main/java/com/hibba/bookly/MainActivity2.kt
@@ -1,23 +1,16 @@
 package com.hibba.bookly
 
-import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        Handler(Looper.getMainLooper()).postDelayed({
-            startActivity(Intent(this, MainActivity2::class.java))
-        }, 4000
-        )
+        setContentView(R.layout.activity_main2)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
