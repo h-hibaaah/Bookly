574e9ec90a54302aa0db2f1a676aeae01445a7df
h-hibaaah <hibba5825@gmail.com>
2026-08-17 19:51:00 +0500

    Updated

--- app/src/main/java/com/hibba/bookly/MainActivity2.kt
+++ app/src/main/java/com/hibba/bookly/MainActivity2.kt
@@ -13,27 +13,6 @@ class MainActivity2 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main2)
-        val button = findViewById<AppCompatButton>(R.id.appCompatButton)
-        button.setOnClickListener {
-            button.animate()
-                .scaleX(0.9f)
-                .scaleY(0.9f)
-                .setDuration(1000)
-                .withEndAction {
-                    button.animate()
-                    .scaleX(1f)
-                    .scaleY(1f)
-                    .setDuration(1000)
-                        .withEndAction {
-                            val intent = Intent(this, MainActivity3::class.java)
-                            startActivity(intent)
-                        }
-                        .start()
-
-                }
-                .start()
-
-        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
