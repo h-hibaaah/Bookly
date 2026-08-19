3397a54e4a9847cec39492d66830a454f6fded57
h-hibaaah <hibba5825@gmail.com>
2026-08-19 18:55:20 +0500

    Kotlin for home fragment added

--- app/src/main/java/com/hibba/bookly/MainActivity3.kt
+++ app/src/main/java/com/hibba/bookly/MainActivity3.kt
@@ -11,6 +11,11 @@ class MainActivity3 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main3)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragmentContainer, HomeFragment())
+                .commit()
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
