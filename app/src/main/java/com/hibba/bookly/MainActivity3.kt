38ca13e11fa7cde6039ff48227d9be4c014f6225
h-hibaaah <hibba5825@gmail.com>
2026-08-20 13:17:59 +0500

    Search Fragment and its working Added

--- app/src/main/java/com/hibba/bookly/MainActivity3.kt
+++ app/src/main/java/com/hibba/bookly/MainActivity3.kt
@@ -5,20 +5,70 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity3 : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main3)
+
+        val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.fragmentContainer, HomeFragment())
                 .commit()
         }
+
+        bottomNavigation.setOnItemSelectedListener { item ->
+
+            when (item.itemId) {
+
+                R.id.nav_home -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainer, HomeFragment())
+                        .commit()
+                    true
+                }
+
+                R.id.nav_explore -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainer, SearchFragment())
+                        .commit()
+                    true
+                }
+//
+//                R.id.nav_library -> {
+//                    supportFragmentManager.beginTransaction()
+//                        .replace(R.id.fragmentContainer, LibraryFragment())
+//                        .commit()
+//                    true
+//                }
+//
+//                R.id.nav_profile -> {
+//                    supportFragmentManager.beginTransaction()
+//                        .replace(R.id.fragmentContainer, ProfileFragment())
+//                        .commit()
+//                    true
+//                }
+
+                else -> false
+            }
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            val systemBars =
+                insets.getInsets(WindowInsetsCompat.Type.systemBars())
+
+            v.setPadding(
+                systemBars.left,
+                systemBars.top,
+                systemBars.right,
+                systemBars.bottom
+            )
+
             insets
         }
     }
